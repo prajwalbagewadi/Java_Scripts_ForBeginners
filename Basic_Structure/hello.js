e28e9d0c51b41c4115739f0341a1f1ac9b2be0f3
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2024-06-02 20:22:46 +0530

    Create hello.js

---
+++ Basic_Structure/hello.js
@@ -0,0 +1,28 @@
+/**
+ * basic structure / basic program of javascript
+ */
+
+// single line comment
+
+/*
+this is 
+a 
+multi line 
+comment
+*/
+
+//variable 
+var a=10; // variable declaration using the var
+let b=20; // variable declaration using the let
+
+//function
+function add(num1,num2){ //function declaration
+    return num1+num2;
+}
+
+//function call
+var result=add(a,b);
+
+//print statement
+// this statement prints the result in the browser's console
+console.log("sum=",result);
